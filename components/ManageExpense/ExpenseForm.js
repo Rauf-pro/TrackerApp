6f7fc42d3abb3314f80e91bcb49221bde8855ca7
rrauf8178 <rrauf8178@gmail.com>
2022-11-16 11:48:23 +0400

    Configuring the Form Input Elements

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -10,9 +10,16 @@ function ExpenseForm() {
        onChangeText:  amountChangedHandler,
       }}/>
       <Input label="Date" textInputConfig={{
-        placeholder: 'YYYY-MM-DD'
+        placeholder: 'YYYY-MM-DD',
+        maxLength: 10,
+        onChangeText: () => {}
       }}/>
-      <Input label="Description"/>
+      <Input label="Description" textInputConfig={{
+        multiline: true,
+        //autoCapitalize: 'none'
+        //autoCorrect: false  // default is true
+
+      }}/>
     </View>
   );
 }
